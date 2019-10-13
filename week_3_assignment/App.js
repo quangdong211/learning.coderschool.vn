a756db77c009ff78ec5bb4a005f85049bb78a8e0
quangdong211 <quangdong211@gmail.com>
2019-10-14 00:15:05 +0700

    [week 3]* Cập nhật lỗi thư viện

--- week_3_assignment/App.js
+++ week_3_assignment/App.js
@@ -2,12 +2,6 @@ import * as React from 'react';
 import { Text, View, StyleSheet, TouchableOpacity, Image } from 'react-native';
 import Constants from 'expo-constants';
 
-// You can import from local files
-import AssetExample from './components/AssetExample';
-
-// or any pure javascript modules available in npm
-import { Card } from 'react-native-paper';
-
 const CHOICES = [
   {
     name: 'rock',
